4744a8eaed8b581403d27f44236026ba09c28a00
o-write <60804751+o-write@users.noreply.github.com>
2026-06-10 02:32:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ def load_and_preprocess_data_full():
 raw_df, df_processed, df_clean, df_original_five_raw, missing_before, initial_rows, initial_cols, missing_after_initial, rows_after_initial_drop, initial_duplicates_count, rows_after_duplicates_drop, missing_before_count, missing_after_initial_count = load_and_preprocess_data_full()
 
 # Load models
-rf_model = pickle.load(open('model_rf.pkl', 'rb'))
+rf_model = pickle.load(open('model_rf (1).pkl', 'rb'))
 nb_model = pickle.load(open('model_nb.pkl', 'rb'))
 reg_model = pickle.load(open('model_reg.pkl', 'rb'))
 reg_scaler_loaded = pickle.load(open('reg_scaler.pkl', 'rb'))
