367f25b2a7e4aaf30bc8eed09f236098c2f8bce8
o-write <60804751+o-write@users.noreply.github.com>
2026-06-11 13:42:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -282,7 +282,7 @@ elif menu == "Eksplorasi Data & Visualisasi":
         best_k_idx = np.argmax(sil_scores_eval)
         best_k_silhouette = ks[best_k_idx]
         st.markdown(f"💡 **Nilai K Terbaik Secara Matematis (Silhouette Score):** `k = {best_k_silhouette}`")
-        st.info("Meskipun Silhouette Score tertinggi berada pada K tertentu, nilai K=4 dipertahankan dalam analisis segmentasi akhir demi kedalaman akomodasi interpretasi profil bisnis ritel.")
+        st.info("Meskipun Silhouette Score tertinggi berada pada K=2 , nilai K=4 dipertahankan dalam analisis segmentasi akhir demi kedalaman akomodasi interpretasi profil bisnis ritel.")
 
         # --- Plot Metode Elbow (hanya satu plot) ---
         st.image('elbow_plot.png', caption='Distortion Score Elbow for KMeans Clustering')
